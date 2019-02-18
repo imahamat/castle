c7d4a36bfa619f2b28477e8bb0ad658e8a4d21a2
Ismail Hour MAHAMAT <35899660+imahamat@users.noreply.github.com>
2019-02-18 14:49:49 +0100

    Update mahamat.js

--- mahamat.js
+++ mahamat.js
@@ -5,15 +5,23 @@ var fs = require('fs');
 const cheerio = require('cheerio');
 var http = require('http');
 const fetch = require('node-fetch');
-var tab = new Object();
 const michelin = require('./michelin');
-var start=michelin.start;
+var starred = michelin.start;
 
-var priceAvailable = new Object();
-var tester=[];
-var	obje= new Object();
 
-//fonction pour recuperer les weekends
+
+//Relais- Chateaux
+async function Relais_Chateaux()
+{
+  var urls = [];
+	await request('https://www.relaischateaux.com/fr/site-map/etablissements',(error, response, html)=> {
+  var $ = cheerio.load(html);
+	$('#countryF').find("h3:contains('France')").parent().find('.listDiamond > li').each( (index, value) => {
+	urls.push($(this).find("a").first()[0].attribs.href);			});		});
+	return urls;
+}
+
+//Recuperer les weekends
 async function Samedi(mois)
   {    var jourMois=[];
 		 for (var i=1;i<32;i++)
@@ -25,13 +33,4 @@ async function Samedi(mois)
 		 return jourMois;
 	}
 
-//fonction pr comparer
-async function Relais_Chateaux()
-{
-  var urls = [];
-	await request('https://www.relaischateaux.com/fr/site-map/etablissements',(error, response, html)=> {
-  var $ = cheerio.load(html);
-	$('#countryF').find("h3:contains('France')").parent().find('.listDiamond > li').each( (index, value) => {
-	urls.push($(this).find("a").first()[0].attribs.href);			});		});
-	return urls;
-}
+
